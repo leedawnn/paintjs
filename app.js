ef20a28cc5cb8dd462a5678eb00da3fac2a26def
leedawn <2g_hee@naver.com>
2021-02-27 23:58:56 +0900

    Add function that brings backgroundColor elements when a color is clicked.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -11,7 +12,7 @@ function stopPainting() {
 }
 
 function startPainting() {
-    painting = ture;
+    painting = true;
 }
 
 function onMouseMove(event) { // 모든 움직임을 감지하고, 라인을 만든다.
@@ -21,13 +22,13 @@ function onMouseMove(event) { // 모든 움직임을 감지하고, 라인을 만
         ctx.beginPath(); // 선을 그리기 위한 메서드 호출
         ctx.moveTo(x, y); // 시작점을 (x,y)좌표로 이동
     } else {
-        ctx.lineTo(); // 마지막 점을 지정된 (x,y) 좌표에 직선으로 연결
-        ctx.stroke(); // 현재 획 스타일로 획 긋기 
+        ctx.lineTo(x, y); // 마지막 점을 지정된 (x,y) 좌표에 직선으로 연결
+        ctx.stroke(); // 현재 획 스타일로 획 긋기
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    console.log(event.target.style); 
 }
 
 if(canvas) {
@@ -36,3 +37,5 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
